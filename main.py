eb932e8c27692495989a714b09e52cd4d2dc2980
U01A05B0 <antonio.carvajal@axians.es>
2021-10-20 14:46:19 +0200

    ADD base Iris Dataset train test

---
+++ main.py
@@ -0,0 +1,65 @@
+from pandas import read_csv
+# from pandas.plotting import scatter_matrix
+# from matplotlib import pyplot
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+#from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+# box and whisker plots
+# dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+# pyplot.show()
+
+# scatter plot matrix
+# scatter_matrix(dataset)
+# pyplot.show()
+
+# Split-out validation dataset
+x = dataset.values[:, 0:4]
+y = dataset.values[:, 4]
+x_train, x_validation, y_train, y_validation = train_test_split(x, y, test_size=0.20, random_state=1)
+
+# Spot Check Algorithms
+# models = []
+# models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+# models.append(('LDA', LinearDiscriminantAnalysis()))
+# models.append(('KNN', KNeighborsClassifier()))
+# models.append(('CART', DecisionTreeClassifier()))
+# models.append(('NB', GaussianNB()))
+# models.append(('SVM', SVC(gamma='auto')))
+
+# evaluate each model in turn
+# results = []
+# names = []
+
+# for name, model in models:
+#     kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+#     cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring='accuracy')
+#     results.append(cv_results)
+#     names.append(name)
+#     print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Compare Algorithms
+# pyplot.boxplot(results, labels=names)
+# pyplot.title('Algorithm Comparison')
+# pyplot.show()
+
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(x_train, y_train)
+predictions = model.predict(x_validation)
+
+# Evaluate predictions
+print(accuracy_score(y_validation, predictions))
+print(confusion_matrix(y_validation, predictions))
+print(classification_report(y_validation, predictions))
